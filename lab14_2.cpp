b4f8d28952e387c6684dece37b10c0b2c2e5f9aa
Panthaweekan <tatanoobza1234@hotmail.com>
2021-12-28 02:02:13 +0700

    FINISH lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,34 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+
+void showImage(const bool photo[][M])
+{
+  cout << "------------------------------------------------------------------------\n";
+    for(int q = 0; q < N; q++){
+        cout << "|";
+        for(int w = 0; w < M ; w++){
+            if(photo[q][w] == 0) cout << " ";
+            if(photo[q][w] == 1) cout << "*";
+        }
+        cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool photo[][M],int z,int x,int y)
+{
+    for(int i = 0; i < N; i++)
+    {
+
+        for(int j = 0; j < M ; j++)
+        {
+            if (sqrt(pow(i - x, 2) + pow(j - y, 2)) + 1 <= z )
+            {
+             photo[i][j]=1;
+            }
+
+        }
+    }
+}
